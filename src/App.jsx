af7590ed50ef87550d042daa8cb514bc89010386
AsitChandraRoy <asitchandraroy@gmail.com>
2024-12-17 23:46:28 +0600

    footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import LatestTrends from "./components/LatestTrends";
 import MyFeedback from "./components/MyFeedback";
 import RecentActivities from "./components/RecentActivities";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <LatestTrends />
       <MyFeedback />
       <RecentActivities />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <section className="px-5 sm:px-11 xl:px-20 py-7 mx-auto border-t">
+        <div className="grid grid-cols-12 gap-4 justify-center items-center">
+          <div className="col-span-12 sm:col-span-4 flex flex-col start-0 justify-self-center sm:-mt-[140px] mt-0">
+            <h3 className="text-[#1E1E1E] text-xl font-extrabold ">About</h3>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">About MyFeedback</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Investor Relations</p>
+          </div>
+          <div className="col-span-12 sm:col-span-4 justify-self-center">
+            <h3 className="text-[#1E1E1E] text-xl font-extrabold ">
+              MyFeedback
+            </h3>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">
+              MyFeedback for business
+            </p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Collections</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Talk</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Events</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">MyFeedback blog</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Support</p>
+            <p className="text-[#5E5E5E] text-sm mt-2 ">Developers</p>
+          </div>
+          <div className="col-span-12 sm:col-span-4 justify-self-center sm:-mt-[110px] mt-0">
+            <h3 className="text-[#1E1E1E] text-xl font-extrabold ">
+              Languages
+            </h3>
+            <div className="flex items-center gap-1">
+              <p className="text-[#5E5E5E] text-sm ">English</p>
+              <img src="downArrow.png" alt="" />
+            </div>
+            <h3 className="text-[#1E1E1E] text-xl font-extrabold mt-5 ">
+              Countries
+            </h3>
+            <div className="flex items-center gap-1">
+              <p className="text-[#5E5E5E] text-sm ">Singapour</p>
+              <img src="downArrow.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="text-[#1E1E1E] text-sm font-semibold text-center mt-8 ">
+          Copyright&copy;Septembre 2023 myfeedback, designed by scott
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Footer;
